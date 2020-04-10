52710fe4335962d4e71c7141fabf2a9e7c3b45cd
Marc Gallagher <marcgallagher@gmail.com>
2020-04-09 22:00:07 -0700

    Configuring eslint, fixing errors

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+	extends: [
+		'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true, // Allows for the parsing of JSX
+		},
+	},
+	rules: {
+		indent: ['error', 'tab'],
+	},
+	settings: {
+		react: {
+			version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+		},
+	},
+};
