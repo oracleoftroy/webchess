e55f87491c35f5ea008babc971c7a91a10a87f38
Marc Gallagher <marcgallagher@gmail.com>
2020-04-11 20:18:57 -0700

    Making web service call and displaying result

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 
+class ApiError extends Error {
+	public status: string;
+
+	constructor(public response: Response) {
+		super(`HTTP Error ${response.statusText}`);
+		this.status = response.statusText;
+	}
+}
+
+async function fetchMsg(): Promise<any> {
+	const response = await fetch('test', {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	if (!(response.status >= 200 && response.status < 300)) {
+		const error = new ApiError(response);
+		error.status = response.statusText;
+		error.response = response;
+		console.log(error);
+		throw error;
+	}
+
+	return await response.json();
+}
+
 function App(): JSX.Element {
+	const [error, setError] = useState<any>(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [msg, setMsg] = useState('');
+
+	useEffect(() => {
+		fetchMsg().then(
+			(result) => {
+				setIsLoaded(true);
+				setMsg(result.msg);
+			},
+			// Note: it's important to handle errors here
+			// instead of a catch() block so that we don't swallow
+			// exceptions from actual bugs in components.
+			(error) => {
+				setIsLoaded(true);
+				setError(error);
+			},
+		);
+	}, []);
+
+	let ajaxMsg = null;
+	if (error != null) {
+		ajaxMsg = <div>Error: {error?.message}</div>;
+	} else if (!isLoaded) {
+		ajaxMsg = <div>Loading...</div>;
+	} else {
+		ajaxMsg = <p>Msg: {msg}</p>;
+	}
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1>This is my header</h1>
 			</header>
+			<section>
+				<h2>Ajax!</h2>
+				<p>Getting message from the server</p>
+				{ajaxMsg}
+			</section>
 			<section>
 				<h2>This is a section of the page</h2>
 				<article>
