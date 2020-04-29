2ca8f761aeb68db17977e5fc37ac79cf19e3f5fb
Marc Gallagher <marcgallagher@gmail.com>
2020-04-29 00:00:46 -0700

    storing websocket in state, not a global

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,24 +1,8 @@
 import './App.scss';
-import React, { useState, useEffect, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent, useCallback } from 'react';
 import { ChessGame } from './ChessGame';
 import { Request, Response, ChatResponse } from './protocol';
 import { Chat } from './Chat';
-
-function createWebSocket(): WebSocket {
-	const uri = process.env.REACT_APP_WSS_BASE_URI || window.location.href.replace(/http/, 'ws');
-	const ws = new WebSocket(uri);
-
-	ws.onclose = () => {
-		console.error(`WebSocket closed, reconnecting...`);
-		setTimeout(createWebSocket, 5000);
-	};
-	// ws.onerror = (e) => {
-	// 	console.error(`WebSocket error: ${e}`);
-	// 	setTimeout(createWebSocket, 10000);
-	// };
-
-	return ws;
-}
 
 function msg(handler: (res: Response) => any): typeof ws.onmessage {
 	return (e: MessageEvent) => {
@@ -26,11 +10,19 @@ function msg(handler: (res: Response) => any): typeof ws.onmessage {
 	};
 }
 
-let ws = createWebSocket();
+const createWebSocket = (setup: (ws: WebSocket) => void): WebSocket => {
+	const uri = process.env.REACT_APP_WSS_BASE_URI || window.location.href.replace(/http/, 'ws');
+	const ws = new WebSocket(uri);
 
-function sendRequest(req: Request): void {
-	ws.send(JSON.stringify(req));
-}
+	setup(ws);
+
+	// ws.onerror = (e) => {
+	// 	console.error(`WebSocket error: ${e}`);
+	// 	setTimeout(createWebSocket, 10000);
+	// };
+
+	return ws;
+};
 
 interface User {
 	id: string | null;
@@ -40,48 +32,76 @@ interface User {
 // TODO: Have some sort of visual overlay when websocket is not open (connecting, closed, error)
 function App(): JSX.Element {
 	//	const [state, setState] = useState('disconnected' as ClientState);
-	const [user, setUser] = useState((): User | null => {
-		const name = localStorage.getItem('username');
-		const id = localStorage.getItem('userid');
-		return name ? { name: name, id: id } : null;
+	const [user, setUser] = useState<User | null>(null);
+	const [chat, setChat] = useState([] as ChatResponse[]);
+
+	const [ws, setWs] = useState(() => {
+		const setup = (ws: WebSocket): void => {
+			ws.onclose = () => {
+				// TODO: should clear chat log or at least display that you have an error...?
+				console.error(`WebSocket closed, reconnecting...`);
+				setTimeout(() => setWs(createWebSocket(setup)), 5000);
+			};
+
+			ws.onmessage = msg((res: Response) => {
+				switch (res.type) {
+					case 'error':
+						console.error(`Received error of type '${res.type}': ${res.msg}`);
+
+						// for errors setting user-info, clear our cached information and
+						// set the user to null to force the user to sign in again
+						if (res.req === 'user-info') {
+							localStorage.removeItem('username');
+							localStorage.removeItem('userid');
+							setUser(null);
+						}
+						break;
+					case 'user-info':
+						setUser((user) => {
+							if (user && user.name === res.username && user.id === res.id) {
+								return user;
+							} else {
+								return { name: res.username, id: res.id };
+							}
+						});
+						break;
+					case 'chat':
+					case 'chat-join':
+					case 'chat-leave':
+						setChat((c) => [...c, res]);
+						break;
+				}
+			});
+		};
+
+		return createWebSocket(setup);
 	});
 
+	const sendRequest = useCallback(
+		(req: Request): void => {
+			ws.send(JSON.stringify(req));
+		},
+		[ws],
+	);
+
 	useEffect(() => {
-		if (user) {
+		// We don't yet have a user, check if we have one stored in local storage, and
+		// if so, sent that to the server
+		if (!user) {
+			const name = localStorage.getItem('username');
+			const id = localStorage.getItem('userid');
+
+			if (name) {
+				sendRequest({ type: 'user-info', username: name, id: id || undefined });
+			}
+		} else if (user) {
+			// user set by response from server, cache in local storage
 			console.log('running user effect');
-			sendRequest({ type: 'user-info', username: user.name, id: user.id });
 			localStorage.setItem('username', user.name);
 			if (user.id) localStorage.setItem('userid', user.id);
 			else localStorage.removeItem('userid');
 		}
-	}, [user]);
-
-	const [chat, setChat] = useState([] as ChatResponse[]);
-
-	useEffect(() => {
-		ws.onmessage = msg((res: Response) => {
-			switch (res.type) {
-				case 'error':
-					console.error(`Recieved error of type '${res.type}': ${res.msg}`);
-
-					if (res.req === 'user-info') {
-						setUser(null);
-					}
-					break;
-				case 'user-info':
-					setUser((user) => {
-						if (user && user.name === res.username && user.id === res.id) return user;
-						else return { name: res.username, id: res.id };
-					});
-					break;
-				case 'chat':
-				case 'chat-join':
-				case 'chat-leave':
-					setChat((c) => [...c, res]);
-					break;
-			}
-		});
-	}, []);
+	}, [user, sendRequest]);
 
 	const onSignIn = (event: FormEvent) => {
 		event.preventDefault();
@@ -92,11 +112,8 @@ function App(): JSX.Element {
 		target.reset();
 
 		const username = data.get('username') as string | null;
-		console.log(`username: ${username}`);
-
 		if (username) {
-			setUser({ name: username });
-			localStorage.setItem('username', username);
+			sendRequest({ type: 'user-info', username: username });
 		}
 	};
 
